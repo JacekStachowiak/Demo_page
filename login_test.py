bc41e27f8327593ed6410a4ba889ee72b9c3e139
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-08-14 16:15:10 +0200

    update

--- login_test.py
+++ login_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -9,5 +10,9 @@ def test_log_in():
     wait = WebDriverWait(driver, 10)
     driver.get('http://seleniumdemo.com/')
     driver.find_element(By.XPATH, '//*[@id="menu-item-22"]/a/span').click()
+    driver.find_element(By.XPATH, '//*[@id="username"]').send_keys('jacekkowalski@gmail.com')
+    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('ZYWzJn8dP2Lgzvi')
+    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(Keys.ENTER)
     
+        
     driver.quit()
